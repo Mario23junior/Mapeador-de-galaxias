270d8a7f3975166031753c9e0b6ec0e37cf5a21b
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-09-23 19:51:26 -0300

    Inserindo string de inclusão

--- src/main/java/dao/GalaxyDao.java
+++ src/main/java/dao/GalaxyDao.java
@@ -1,16 +1,19 @@
 package dao;
 
+import java.sql.Connection;
+
+import db.ConexaoMachin;
 import model.Galaxy;
 
 public class GalaxyDao implements OperacaoCrudAssinatur {
-   
-	
-	  public static void cricaoGalaxy(Galaxy galaxy) {
-		  
-		  
-	  };
-	  
-	  
+
+	private static Connection connection = ConexaoMachin.createConnecton();
+	private static String sql;
+
+	public static void cricaoGalaxy(Galaxy galaxy) {
+		String sql = "insert into Galaxy (nome, img,ano,magnitude, qtproxima, distancia , constelacao , descricao, tipo) values (?,?,?,?,?,?,?,?,?)";
+	};
+
 //	  public static void eclusaoGalaxy(int galaxiId) {};
 //	  public static Galaxy buscaIdGalaxy() {return null;}
 //	  public static List<Galaxy> GalaxiaBuscaPor(String galaxyPesqu){return null;}
