bda035e025ef0cb96585e3fb9ae0c2cd264bf5a0
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-09-23 18:57:10 -0300

    Criando servelet

--- src/main/java/dao/GalaxyDao.java
+++ src/main/java/dao/GalaxyDao.java
@@ -1,5 +1,18 @@
 package dao;
 
-public class GalaxyDao implements OperacaoCrudAssinatur{
+import model.Galaxy;
 
+public class GalaxyDao implements OperacaoCrudAssinatur {
+   
+	
+	  public static void cricaoGalaxy(Galaxy galaxy) {
+		  
+		  
+	  };
+	  
+	  
+//	  public static void eclusaoGalaxy(int galaxiId) {};
+//	  public static Galaxy buscaIdGalaxy() {return null;}
+//	  public static List<Galaxy> GalaxiaBuscaPor(String galaxyPesqu){return null;}
+//	  public static void galaxyUpdate(Galaxy galaxy){};
 }
