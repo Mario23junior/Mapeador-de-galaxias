acc8169ff7512bcc64f53a2be8c2b216c0746d3c
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-09-23 18:39:43 -0300

    Criando modelo e classe de conexao

---
+++ src/main/java/db/ConexaoMachin.java
@@ -0,0 +1,30 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMachin {
+
+	private static final String urlAcess = "jdbc:mysql://localhost:3306/galaxyMapper";
+	private static final String user = "root";
+	private static final String senha = "P@ssw0ord2";
+
+	public static Connection createConnecton() {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+              System.out.println("Driver não encontrado "+ e.getMessage());
+		}
+		
+		try {
+			Connection conexao = DriverManager.getConnection(urlAcess, user, senha);
+			System.out.println("COnectado com sucesso");
+			return conexao;
+		} catch (SQLException e) {
+			System.out.println("Erro ao conectar com o banco de dados "+ e.getMessage());
+			return null;
+		}
+	}
+}
---
+++ src/main/java/model/Galaxy.java
@@ -0,0 +1,95 @@
+package model;
+
+public class Galaxy {
+
+	private int id;
+	private String nome;
+	private String img;
+	private String ano;
+	private String magnitude;
+	private String qtproxima;
+	private String distancia;
+	private String constelacao;
+	private String descricao;
+	private String tipo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getMagnitude() {
+		return magnitude;
+	}
+
+	public void setMagnitude(String magnitude) {
+		this.magnitude = magnitude;
+	}
+
+	public String getQtproxima() {
+		return qtproxima;
+	}
+
+	public void setQtproxima(String qtproxima) {
+		this.qtproxima = qtproxima;
+	}
+
+	public String getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(String distancia) {
+		this.distancia = distancia;
+	}
+
+	public String getConstelacao() {
+		return constelacao;
+	}
+
+	public void setConstelacao(String constelacao) {
+		this.constelacao = constelacao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
