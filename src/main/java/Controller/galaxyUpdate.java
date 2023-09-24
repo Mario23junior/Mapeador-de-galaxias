0e448f83b04fee94d88aff9e45758739881f064f
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-09-23 23:49:31 -0300

    Criando servlet update

---
+++ src/main/java/Controller/galaxyUpdate.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+ 
+@WebServlet("/galaxyUpdate")
+public class galaxyUpdate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public galaxyUpdate() {
+        super();
+     }
+
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 		doGet(request, response);
+	}
+
+}
